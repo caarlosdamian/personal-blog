b6029e5d7c6d4a1d0414573bcb7bea6e4ec64621
caarlosdamian <dapecarlos@gmail.com>
2025-05-18 18:07:13 -0600

    change env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -12,7 +12,7 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI!;
+  const MONGODB_URI = process.env.MONGODB_URI!;
   if (!MONGODB_URI) {
     throw new Error(
       'Please define the MONGODB_URI environment variable inside .env.local'
